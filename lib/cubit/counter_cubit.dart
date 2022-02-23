f2aa8556d72af997e0cf32619f548d40f1312cec
unknown <shebinpr126@gmail.com>
2022-02-23 10:20:09 +0530

    cubit

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,15 @@
+import 'package:bloc/bloc.dart';
+
+part 'counter_state.dart';
+
+class CounterCubit extends Cubit<CounterStates> {
+  CounterCubit() : super(CounterStates(counterValue: 0));
+
+  void increment() {
+    emit(CounterStates(counterValue: state.counterValue + 1));
+  }
+
+  void decrement() {
+    emit(CounterStates(counterValue: state.counterValue - 1));
+  }
+}
---
+++ lib/cubit/counter_state.dart
@@ -0,0 +1,8 @@
+part of 'counter_cubit.dart';
+
+class CounterStates {
+  int counterValue;
+  CounterStates({
+    required this.counterValue,
+  });
+}
